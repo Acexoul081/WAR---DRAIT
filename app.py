536089f2c8bf1fbfb44554133e8c9b9c4a2f793f
Acexoul081 <williamchang08.wz@gmail.com>
2022-12-29 17:29:00 +0700

    cron update, visualize threshold, change threshold feature

--- app.py
+++ app.py
@@ -91,20 +91,19 @@ def main_page():
 def show_metric(metric):
     global first_index, last_index
     if request.method == 'GET':
+        session['threshold'] = 'static'
         decoded_metric = base64.urlsafe_b64decode(metric).decode("ascii")
         value, loss = get_value(metric)
-        session['threshold_value'] = get_static_threshold(metric)
 
-        anomalies = get_anomalies(metric, loss)
+        anomalies, threshold = get_anomalies(metric, loss)
         value_anomalies = value.iloc[anomalies]
         loss_anomalies = loss.iloc[anomalies]
         model_version = get_model_version(metric)
 
-        stdin,stdout,stderr = client.exec_command(f"crontab -l | grep \"{decoded_metric}\"")
+        stdin,stdout,stderr = client.exec_command(f"crontab -l | grep -F \"{decoded_metric}\"")
         cron_info = stdout.read().decode('utf-8').strip().split('\n')
         cron_list = []
-        print(decoded_metric)
-        print(len(cron_info))
+
         if len(cron_info) > 0:
             print(cron_info)
             for i in cron_info:
@@ -116,7 +115,7 @@ def show_metric(metric):
                     })
 
         val_graph_json = create_value_graph(value, value_anomalies, 'metric_value')
-        loss_graph_json = create_value_graph(loss, loss_anomalies, 'loss')
+        loss_graph_json = create_value_graph(loss, loss_anomalies, 'loss', threshold)
         preproc_graph_json = create_value_graph(loss, loss_anomalies, 'value')
         
         decoded_metric_in_list = decoded_metric.split('|')
@@ -124,39 +123,57 @@ def show_metric(metric):
             'ip':decoded_metric_in_list[0],
             'source':decoded_metric_in_list[1],
             'name':decoded_metric_in_list[2],
-            'type':decoded_metric_in_list[3]
+            'type':decoded_metric_in_list[3],
+            'key': metric
         }
         
         return render_template('metric.html', metric=metric_detail, valueGraph=val_graph_json, lossGraph=loss_graph_json, preprocGraph= preproc_graph_json, modelStatus=model_version, crons = cron_list if cron_info else 'No Cron Available')
 
-    elif request.method == 'POST':   
-        value, loss = get_value(metric)
-        anomalies = get_anomalies(metric, loss)
-        value_anomalies = value.iloc[anomalies]
-        loss_anomalies = loss.iloc[anomalies]
+    elif request.method == 'POST':
+        return get_value_json(metric)
 
-        value_datetime = [i.to_pydatetime() for i in value['metric_datetime']]
-        loss_datetime = [i.to_pydatetime() for i in loss['metric_datetime']]
-        #ganti universal anomalies ntar
-        value_anom_datetime = [i.to_pydatetime() for i in value_anomalies['metric_datetime']]
-        loss_anom_datetime = [i.to_pydatetime() for i in loss_anomalies['metric_datetime']]
-        return {
-            'x_value':json.dumps(value_datetime, default=str),
-            'y_value': json.dumps(value['metric_value'].values.tolist()), 
-            'x_value_anom': json.dumps(value_anom_datetime, default=str), 
-            'y_value_anom':json.dumps(value_anomalies['metric_value'].values.tolist()),
-            'x_loss':json.dumps(loss_datetime, default=str),
-            'y_loss': json.dumps(loss['loss'].values.tolist()), 
-            'x_loss_anom': json.dumps(loss_anom_datetime, default=str), 
-            'y_loss_anom':json.dumps(loss_anomalies['loss'].values.tolist())
-        }
+def get_value_json(metric):
+    value, loss = get_value(metric)
+    anomalies, threshold = get_anomalies(metric, loss)
+    value_anomalies = value.iloc[anomalies]
+    loss_anomalies = loss.iloc[anomalies]
 
-def create_value_graph(dataset, anomalies, target_column):
+    value_datetime = [i.to_pydatetime() for i in value['metric_datetime']]
+    loss_datetime = [i.to_pydatetime() for i in loss['metric_datetime']]
+    #ganti universal anomalies ntar
+    value_anom_datetime = [i.to_pydatetime() for i in value_anomalies['metric_datetime']]
+    loss_anom_datetime = [i.to_pydatetime() for i in loss_anomalies['metric_datetime']]
+    return {
+        'x_value':json.dumps(value_datetime, default=str),
+        'y_value': json.dumps(value['metric_value'].values.tolist()), 
+        'x_value_anom': json.dumps(value_anom_datetime, default=str), 
+        'y_value_anom':json.dumps(value_anomalies['metric_value'].values.tolist()),
+        'x_loss':json.dumps(loss_datetime, default=str),
+        'y_loss': json.dumps(loss['loss'].values.tolist()), 
+        'x_loss_anom': json.dumps(loss_anom_datetime, default=str), 
+        'y_loss_anom':json.dumps(loss_anomalies['loss'].values.tolist())
+    }
+
+def create_value_graph(dataset, anomalies, target_column, threshold=None):
     dataset = dataset.set_index('metric_datetime')
     dataset = dataset.resample('1T').mean()
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=dataset.index, y=dataset[target_column], name='Value Time Series'))
     fig.add_trace(go.Scatter(x=anomalies['metric_datetime'], y=anomalies[target_column], mode='markers', name='Anomaly'))
+    if threshold:
+        if session['threshold'] == 'static':
+            fig.add_hline(y=threshold)
+        else:
+            for start_idx, end_idx, limit in threshold:
+                fig.add_shape(type='line',
+                    x0=dataset.index[start_idx],
+                    y0=limit,
+                    x1=dataset.index[end_idx],
+                    y1=limit,
+                    line=dict(color='Red',),
+                    xref='x',
+                    yref='y'
+                )
     fig.update_layout(showlegend=True, title=target_column)
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
@@ -218,25 +235,23 @@ def get_anomalies(metric, dataset):
         if threshold is not None:
             dataset['anomaly'] = dataset['loss'].ge(threshold)
     elif session['threshold'] == 'dynamic':
-        threshold = get_dynamic_threshold(metric, dataset.iloc[-1]['metric_datetime'].to_pydatetime())
+        anom, threshold = get_dynamic_threshold(metric, dataset.iloc[-1]['metric_datetime'].to_pydatetime(),dataset['loss'])
         
-        for start_index, end_index in zip(threshold['start_time'],threshold['end_time']):
+        for start_index, end_index in zip(anom['start_time'],anom['end_time']):
             mask = (dataset['metric_datetime'] > start_index) & (dataset['metric_datetime'] <= end_index)
             dataset.loc[mask,'anomaly'] = True
-    
+
     anomalies = dataset.loc[dataset['anomaly'] == True].index
-    return anomalies
+    return anomalies, threshold
 
 @app.route('/static', methods=['POST'])
 def change_static_threshold():
-    metric = request.args.get('data')
-    session['threshold_value'] = get_static_threshold(metric)
-    
-    return '', 204
+    metric = request.form['metric']
+    session['threshold'] = 'static'
+    return get_value_json(metric)
 
 def get_static_threshold(metric):
     db_session = Session()
-    session['threshold'] = 'static'
     if metric in infra_metadata:
         threshold_metric_key = infra_metadata[metric]['key']
     else:
@@ -251,11 +266,11 @@ def get_static_threshold(metric):
 
 @app.route('/dynamic', methods=['POST'])
 def change_dynamic_threshold():
-    metric = request.args.get('data')
+    metric = request.form['metric']
     session['threshold'] = 'dynamic'
-    return '', 204
+    return get_value_json(metric)
 
-def get_dynamic_threshold(metric, first_date):
+def get_dynamic_threshold(metric, first_date, loss):
     db_session = Session()
     if metric in infra_metadata:
         anomaly_metric_key = infra_metadata[metric]['key']
@@ -264,17 +279,18 @@ def get_dynamic_threshold(metric, first_date):
         
     threshold_query = db_session.query(db.anomalies).filter(
         and_(
-            db.anomalies.metric_key == infra_metadata[metric]['key'],
+            db.anomalies.metric_key == anomaly_metric_key,
             db.anomalies.end_time > first_date
         )   
         ).order_by(desc(db.anomalies.start_time))
     dynamic_threshold = pd.read_sql(threshold_query.statement, threshold_query.session.bind)
-    return dynamic_threshold
+
+    dynamic_error = Errors(loss)
+    epsilon = dynamic_error.process_batches()
+    return dynamic_threshold, epsilon
 
 @app.route('/update-cron', methods=['POST'])
 def update_cron_tab():
-    data = request.args.get('data')
-    print(data)
     new_cron = request.form['new-cron']
     prev_cron = request.form['prev-cron']
     new_cron_script = prev_cron.replace(prev_cron[0 : len(new_cron)], new_cron, 1)
@@ -287,3 +303,220 @@ def update_cron_tab():
 @app.route('/about-us')
 def show_aboutus():
     return render_template('about-us.html')
+
+import more_itertools as mit
+import math
+
+l_s = 60
+batch_size = 32
+window_size= 30
+smoothing_perc= 0.05
+error_buffer= 100
+p= 0.13
+class Errors:
+    def __init__(self, e):
+        self.window_size = window_size
+        self.n_windows = int((e.shape[0] -
+                              (batch_size * self.window_size))
+                             / batch_size)
+        self.i_anom = np.array([])
+        self.E_seq = []
+        self.E_point = []
+        self.anom_scores = []
+        self.train_from_begin = True
+
+        self.e = e
+        smoothing_window = int(batch_size * window_size
+                               * smoothing_perc)
+
+        # smoothed prediction error
+        self.e_s = pd.DataFrame(self.e).ewm(span=smoothing_window)\
+            .mean().values.flatten()
+
+    def adjust_window_size(self, e):
+        """
+        Decrease the historical error window size (h) if number of test
+        values is limited.
+
+        Args:
+            channel (obj): Channel class object containing train/test data
+                for X,y for a single channel
+        """
+
+        while self.n_windows < 0:
+            self.window_size -= 1
+            self.n_windows = int((e.shape[0]
+                                 - (batch_size * self.window_size))
+                                 / batch_size)
+            if self.window_size == 1 and self.n_windows < 0:
+                raise ValueError('Batch_size ({}) larger than y_test (len={}). '
+                                 'Adjust in config.yaml.'
+                                 .format(batch_size,
+                                         e.shape[0]))
+
+    def process_batches(self):
+        """
+        Top-level function for the Error class that loops through batches
+        of values for a channel.
+
+        Args:
+            channel (obj): Channel class object containing train/test data
+                for X,y for a single channel
+        """
+        threshold_array = []
+        self.adjust_window_size(self.e)
+        for i in range(0, self.n_windows+1):
+            prior_idx = i * batch_size
+            idx = (window_size * batch_size) + (i * batch_size)
+            if i == self.n_windows:
+                idx = self.e.shape[0]
+            
+            window = ErrorWindow(prior_idx, idx, self, self.n_windows)
+
+            window.find_epsilon()
+            window.find_epsilon(inverse=True)
+            threshold_array.append([prior_idx, idx,window.epsilon])
+        return threshold_array   
+
+class ErrorWindow:
+    def __init__(self, start_idx, end_idx, errors, window_num):
+        """
+        Data and calculations for a specific window of prediction errors.
+        Includes finding thresholds, pruning, and scoring anomalous sequences
+        for errors and inverted errors (flipped around mean) - significant drops
+        in values can also be anomalous.
+
+        Args:
+            channel (obj): Channel class object containing train/test data
+                for X,y for a single channel
+            config (obj): Config object containing parameters for processing
+            start_idx (int): Starting index for window within full set of
+                channel test values
+            end_idx (int): Ending index for window within full set of channel
+                test values
+            errors (arr): Errors class object
+            window_num (int): Current window number within channel test values
+
+        Attributes:
+            i_anom (arr): indices of anomalies in window
+            i_anom_inv (arr): indices of anomalies in window of inverted
+                telemetry values
+            E_seq (arr of tuples): array of (start, end) indices for each
+                continuous anomaly sequence in window
+            E_seq_inv (arr of tuples): array of (start, end) indices for each
+                continuous anomaly sequence in window of inverted telemetry
+                values
+            non_anom_max (float): highest smoothed error value below epsilon
+            non_anom_max_inv (float): highest smoothed error value below
+                epsilon_inv
+            config (obj): see Args
+            anom_scores (arr): score indicating relative severity of each
+                anomaly sequence in E_seq within a window
+            window_num (int): see Args
+            sd_lim (int): default number of standard deviations to use for
+                threshold if no winner or too many anomalous ranges when scoring
+                candidate thresholds
+            sd_threshold (float): number of standard deviations for calculation
+                of best anomaly threshold
+            sd_threshold_inv (float): same as above for inverted channel values
+            e_s (arr): exponentially-smoothed prediction errors in window
+            e_s_inv (arr): inverted e_s
+            sd_e_s (float): standard deviation of e_s
+            mean_e_s (float): mean of e_s
+            epsilon (float): threshold for e_s above which an error is
+                considered anomalous
+            epsilon_inv (float): threshold for inverted e_s above which an error
+                is considered anomalous
+            y_test (arr): Actual telemetry values for window
+            sd_values (float): st dev of y_test
+            perc_high (float): the 95th percentile of y_test values
+            perc_low (float): the 5th percentile of y_test values
+            inter_range (float): the range between perc_high - perc_low
+            num_to_ignore (int): number of values to ignore initially when
+                looking for anomalies
+        """
+
+        self.i_anom = np.array([])
+        self.E_seq = np.array([])
+        self.non_anom_max = -1000000
+        self.i_anom_inv = np.array([])
+        self.E_seq_inv = np.array([])
+        self.non_anom_max_inv = -1000000
+
+        self.anom_scores = []
+
+        self.window_num = window_num
+
+        self.sd_lim = 12.0
+        self.sd_threshold = self.sd_lim
+        self.sd_threshold_inv = self.sd_lim
+
+        self.e_s = errors.e_s[start_idx:end_idx]
+
+        self.mean_e_s = np.mean(self.e_s)
+        self.sd_e_s = np.std(self.e_s)
+        self.e_s_inv = np.array([self.mean_e_s + (self.mean_e_s - e)
+                                 for e in self.e_s])
+
+        self.epsilon = self.mean_e_s + self.sd_lim * self.sd_e_s
+        self.epsilon_inv = self.mean_e_s + self.sd_lim * self.sd_e_s
+
+        # ignore initial error values until enough history for processing
+        self.num_to_ignore = l_s * 2
+
+
+    def find_epsilon(self, inverse=False):
+        """
+        Find the anomaly threshold that maximizes function representing
+        tradeoff between:
+            a) number of anomalies and anomalous ranges
+            b) the reduction in mean and st dev if anomalous points are removed
+            from errors
+        (see https://arxiv.org/pdf/1802.04431.pdf)
+
+        Args:
+            inverse (bool): If true, epsilon is calculated for inverted errors
+        """
+        e_s = self.e_s if not inverse else self.e_s_inv
+
+        max_score = -10000000
+
+        for z in np.arange(2.5, self.sd_lim, 0.5):
+            epsilon = self.mean_e_s + (self.sd_e_s * z)
+
+            pruned_e_s = e_s[e_s < epsilon] #ambil error yg aman
+
+            i_anom = np.argwhere(e_s >= epsilon).reshape(-1,)
+            buffer = np.arange(1, error_buffer)
+            i_anom = np.sort(np.concatenate((i_anom,
+                                            np.array([i+buffer for i in i_anom])
+                                             .flatten(),
+                                            np.array([i-buffer for i in i_anom])
+                                             .flatten())))
+            i_anom = i_anom[(i_anom < len(e_s)) & (i_anom >= 0)]
+            i_anom = np.sort(np.unique(i_anom))
+
+            if len(i_anom) > 0:
+                # group anomalous indices into continuous sequences
+                groups = [list(group) for group
+                          in mit.consecutive_groups(i_anom)]
+                E_seq = [(g[0], g[-1]) for g in groups if not g[0] == g[-1]]
+
+                mean_perc_decrease = (self.mean_e_s - np.mean(pruned_e_s)) \
+                                     / self.mean_e_s
+                sd_perc_decrease = (self.sd_e_s - np.std(pruned_e_s)) \
+                                   / self.sd_e_s
+                score = (mean_perc_decrease + sd_perc_decrease) \
+                        / (len(E_seq) ** 2 + len(i_anom))
+
+                # sanity checks / guardrails
+                if score >= max_score and len(E_seq) <= 5 and \
+                        len(i_anom) < (len(e_s) * 0.5):
+                    max_score = score
+                    if not inverse:
+                        self.sd_threshold = z
+                        self.epsilon = self.mean_e_s + z * self.sd_e_s
+                    else:
+                        self.sd_threshold_inv = z
+                        self.epsilon_inv = self.mean_e_s + z * self.sd_e_s
+
--- static/metric/index.js
+++ static/metric/index.js
@@ -1,7 +1,6 @@
 function update_graph(metric){
     $.getJSON({
-        url:'/metric', data:metric, success:function(result){
-            
+        url:'/metric', data:metric, success:function(result){   
             Plotly.newplot('value', result, {})
             Plotly.newplot('loss', result, {})
         }
@@ -16,29 +15,36 @@ $('button').on('click', function(){
 $('#static-btn').addClass('selected');
 
 $('#static-btn').click(function(){
-    // get metric data here
     $.ajax({
         type: "POST",
         url: '/static',
+        data: {'metric':$(this).val()},
         success: function(result) {
-            console.log('update success')
+            value_update = {x: [JSON.parse(result.x_value), JSON.parse(result.x_value_anom)], y: [JSON.parse(result.y_value), JSON.parse(result.y_value_anom)]}
+            loss_update = {x: [JSON.parse(result.x_loss), JSON.parse(result.x_loss_anom)], y: [JSON.parse(result.y_loss), JSON.parse(result.y_loss_anom)]}
+            Plotly.update('value', value_update, {})
+            Plotly.update('loss', loss_update, {})
         }
     });
 });
 
 $('#dynamic-btn').click(function(e){
+    console.log($(this).val())
     $.ajax({
         type: "POST",
         url: '/dynamic',
+        data: {'metric':$(this).val()},
         success: function(result) {
-            console.log('update success')
+            value_update = {x: [JSON.parse(result.x_value), JSON.parse(result.x_value_anom)], y: [JSON.parse(result.y_value), JSON.parse(result.y_value_anom)]}
+            loss_update = {x: [JSON.parse(result.x_loss), JSON.parse(result.x_loss_anom)], y: [JSON.parse(result.y_loss), JSON.parse(result.y_loss_anom)]}
+            Plotly.update('value', value_update, {})
+            Plotly.update('loss', loss_update, {})
         }
     });
 });
 
 $('.update-cron-btn').click(function(e){
     e.preventDefault();
-    console.log($(this).parent().serialize())
     $.ajax({
         type: "POST",
         url: '/update-cron',
